cba89974a18a8c034852d6f68df0e114e4fd900d
Gedridze <edvinas.kilciauskas@gmail.com>
2024-02-21 13:15:14 +0200

    more update

--- script.js
+++ script.js
@@ -18,7 +18,7 @@
 	}
 }) */
 
-const clientUrl = 'http://https://sp-907-sticky-overlay.sportsbook-iframe.dev.advbet.com/';
+const clientUrl = 'https://sp-907-sticky-overlay.sportsbook-iframe.dev.advbet.com';
 const script = document.createElement('script');
 script.onload = function () {
 	window.iFrameResize({log: false}, '#iframe') // Onload logic for IFrame init
