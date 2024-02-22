b9dd052f7ce16eb5726731a8a31ebbb76e9069e2
Gedridze <edvinas.kilciauskas@gmail.com>
2024-02-22 09:13:00 +0200

    remove logs

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ function createIFrame() {
 	iFrame.style = 'width: 100%'
 	iFrame.frameBorder = 0;
 	document.getElementById('header').after(iFrame);
-	window.iFrameResize({log: true, checkOrigin: false}, '#iframe') // Onload logic for IFrame init
+	window.iFrameResize({log: false, checkOrigin: false}, '#iframe') // Onload logic for IFrame init
 }
 
 
