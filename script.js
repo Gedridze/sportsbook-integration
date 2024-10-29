edf56c4ad1b52e7fd0c656630e50e433feac76c0
david <dovydas.bykovas@adv.bet>
2024-10-29 12:44:43 +0200

    STICKY REWORK EXAMPLE

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ window.addEventListener("message", (event) => {
   if (!event.data.topic) return;
   console.log(event.data.topic, event.data.payload);
 	switch(event.data.topic) {
+                case 'heightChange':
+                  updateIframeHeight(event.data.payload.height);
+                        break;
 		case 'redirect': 
 			redirectPage(event.data.payload.url);
 			break;
@@ -17,40 +20,27 @@ function redirectPage(url) {
 }
 
 const clientUrl = "https://sportsbook.adv.bet/panel-dev";
-const script = document.createElement("script");
-script.onload = function () {
+window.onload = function () {
   createIFrame();
 };
+document.onscroll = function () {
+  const iFrame = document.getElementById("iframe").contentWindow;
+  if (iFrame) {
+    iFrame.postMessage(
+      {
+        topic: "scroll",
+        payload: {
+          scrollDistance: -wrapper.getBoundingClientRect().top,
+        },
+      },
+      "*"
+    );
+  }
+};
 
-script.src = `${clientUrl}/iframeResizer.min.js?${Date.now()}`;
-document.head.appendChild(script);
 function updateIframeHeight(height) {
-  const iframe = document.getElementById("iframe");
+  const iframe = document.getElementById("wrapper");
   iframe.style.height = `${height}px`;
-}
-
-function sendViewPortInfo() {
-  const iframe = document.getElementById("iframe");
-  const payload = {
-    height: window.innerHeight - 100,
-    scrollY: window.scrollY,
-    iframeTopOffset: iframe.offsetTop,
-  };
-
-  iframe.contentWindow.postMessage({ topic: "get_viewport", payload }, "*");
-}
-
-function subscribeScroll() {
-  document.addEventListener("scroll", handleScroll);
-  document.addEventListener("resize", handleScroll);
-}
-function unsubscribeScroll() {
-  document.removeEventListener("scroll", handleScroll);
-  document.removeEventListener("resize", handleScroll);
-}
-
-function handleScroll() {
-  sendViewPortInfo();
 }
 
 function createIFrame() {
@@ -63,14 +53,10 @@ function createIFrame() {
   iFrame = document.createElement("iframe");
   iFrame.src = url;
   iFrame.id = "iframe";
-  iFrame.style = "width: 100%; margin-top: 150px;";
+  iFrame.style = "width: 100%; height: 100dvh;";
   iFrame.frameBorder = 0;
   iFrame.setAttribute('allow', "web-share; clipboard-write;");
-  document.getElementById("header").after(iFrame);
-  window.iFrameResize(
-    { log: false, checkOrigin: false, stickyHeaderHeight: 150 },
-    "#iframe"
-  ); // Onload logic for IFrame init
+  document.getElementById("wrapper").appendChild(iFrame);
 }
 
 function setOdds(string) {
